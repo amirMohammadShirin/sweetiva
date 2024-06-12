ea74e8943dbd97408fed9bba1355fe55db9c293f
amirMohammadShirin <12345678>
2024-06-12 11:22:27 +0330

    create retrofit and okhttp

--- core/network/src/main/kotlin/com/sweet/iva/core/network/helper/RetrofitHelper.kt
+++ core/network/src/main/kotlin/com/sweet/iva/core/network/helper/RetrofitHelper.kt
@@ -1,7 +1,26 @@
 package com.sweet.iva.core.network.helper
 
+import com.squareup.moshi.Moshi
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
 /**
  * Created by aShirin on 6/12/2024.
  */
-class RetrofitHelper {
+object RetrofitHelper {
+
+    fun createRetrofit(
+        baseUrl: String,
+        okHttpClient: OkHttpClient
+    ): Retrofit =
+        Retrofit
+            .Builder()
+            .apply {
+                client(okHttpClient)
+                baseUrl(baseUrl)
+                addConverterFactory(MoshiConverterFactory.create(Moshi.Builder().build()))
+            }
+            .build()
+
 }
