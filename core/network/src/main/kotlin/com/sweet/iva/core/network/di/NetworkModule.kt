21d9170a6d47a2ed11f75993bbc4e32203841590
amirMohammadShirin <12345678>
2024-06-12 11:22:27 +0330

    create retrofit and okhttp

--- core/network/src/main/kotlin/com/sweet/iva/core/network/di/NetworkModule.kt
+++ core/network/src/main/kotlin/com/sweet/iva/core/network/di/NetworkModule.kt
@@ -1,10 +1,40 @@
 package com.sweet.iva.core.network.di
 
+import com.sweet.arch.core.domain.datasource.remote.AuthenticationRemoteDataSource
+import com.sweet.iva.core.network.datasource.AuthenticationRemoteDataSourceImpl
+import com.sweet.iva.core.network.helper.OkHttpHelper
+import com.sweet.iva.core.network.helper.RetrofitHelper
+import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+
+private const val BASE_URL = "http://baseurl.com"
 
 @Module
 @InstallIn(SingletonComponent::class)
-object NetworkModule {
+abstract class NetworkModule {
+
+
+    companion object {
+
+        @Provides
+        fun provideDefaultRetrofit(): Retrofit {
+            return RetrofitHelper.createRetrofit(
+                BASE_URL,
+                OkHttpHelper.createClient(
+                    40
+                )
+            )
+        }
+
+
+    }
+
+    @Binds
+    abstract fun bindAAuthenticationRemoteDataSource(authenticationRemoteDataSourceImpl: AuthenticationRemoteDataSourceImpl): AuthenticationRemoteDataSource
+
 }
