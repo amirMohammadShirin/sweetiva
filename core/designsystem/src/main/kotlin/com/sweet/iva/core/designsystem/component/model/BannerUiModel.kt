07f9841ffd932df902278371ec2a99099904e762
amirMohammadShirin <12345678>
2024-07-07 14:10:36 +0330

    horizontal banner list

---
+++ core/designsystem/src/main/kotlin/com/sweet/iva/core/designsystem/component/model/BannerUiModel.kt
@@ -0,0 +1,11 @@
+package com.sweet.iva.core.designsystem.component.model
+
+/**
+ * Created by aShirin on 7/7/2024.
+ */
+//ToDo: change image type to string and use it as image url !!
+data class BannerUiModel(
+    val id: String,
+    val img: Int,
+    val route: String? = null
+)
