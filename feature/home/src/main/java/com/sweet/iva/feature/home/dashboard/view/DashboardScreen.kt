ba37f9037af1715f6334c66011a7f4d1789d108a
amir mohammad shirin <45653153+amirMohammadShirin@users.noreply.github.com>
2024-07-21 14:26:17 +0330

    mocking shimmer for user accounts

--- feature/home/src/main/java/com/sweet/iva/feature/home/dashboard/view/DashboardScreen.kt
+++ feature/home/src/main/java/com/sweet/iva/feature/home/dashboard/view/DashboardScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -11,10 +12,12 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.sweet.iva.core.designsystem.component.AppBackground
+import com.sweet.iva.core.designsystem.component.AppShimmer
 import com.sweet.iva.core.designsystem.component.AppToolbar
 import com.sweet.iva.core.designsystem.component.HorizontalBannerList
 import com.sweet.iva.core.designsystem.component.HorizontalFeatureList
@@ -33,139 +36,156 @@ import com.sweet.iva.feature.home.dashboard.model.DashboardEvent
 import com.sweet.iva.feature.home.dashboard.model.DashboardUiModel
 import com.sweet.iva.feature.home.dashboard.viewmodel.DashboardViewModel
 
-class DashboardScreen : BaseScreen<DashboardUiModel, DashboardAction, DashboardEvent>(
-    route = ApplicationRoutes.dashboardScreenRoute,
-    name = "خانه"
-) {
+class DashboardScreen :
+    BaseScreen<DashboardUiModel, DashboardAction, DashboardEvent>(
+        route = ApplicationRoutes.dashboardScreenRoute,
+        name = "خانه",
+    ) {
     @Composable
     override fun viewModel(): DashboardViewModel = hiltViewModel()
 
     @Composable
     override fun Content(state: DashboardUiModel) {
-
         val viewModel = viewModel()
 
-
         ConstraintLayout(
-            modifier = Modifier
-                .fillMaxSize()
+            modifier =
+                Modifier
+                    .fillMaxSize(),
         ) {
-
             val (
                 toolbarRef,
-                cardsRef,
-                frequentFeaturesRef,
-                bannerRef,
-                bankFeaturesRef,
-                carFeaturesRef,
-                contentRef
+                contentRef,
             ) = createRefs()
 
             DashboardToolbar(
-                modifier = Modifier.constrainAs(toolbarRef) {
-                    top.linkTo(parent.top, MaterialTheme.dimens.defaultGap)
-                    end.linkTo(parent.end)
-                    start.linkTo(parent.start)
-                    width = Dimension.fillToConstraints
-                },
+                modifier =
+                    Modifier.constrainAs(toolbarRef) {
+                        top.linkTo(parent.top, MaterialTheme.dimens.defaultGap)
+                        end.linkTo(parent.end)
+                        start.linkTo(parent.start)
+                        width = Dimension.fillToConstraints
+                    },
                 onLeftIconClicked = {
                     viewModel.test()
                 },
                 onRightIconClicked = {
                     viewModel.test()
-                }
+                },
             )
 
             Column(
-                modifier = Modifier
-                    .constrainAs(contentRef) {
-                        top.linkTo(toolbarRef.bottom, MaterialTheme.dimens.smallGap)
-                        start.linkTo(parent.start)
-                        end.linkTo(parent.end)
-                        bottom.linkTo(parent.bottom, MaterialTheme.dimens.smallGap)
-                        width = Dimension.fillToConstraints
-                        height = Dimension.fillToConstraints
-                    }
-                    .verticalScroll(rememberScrollState())
-                    .padding(horizontal = MaterialTheme.dimens.defaultPadding)
-                ,
-                verticalArrangement = Arrangement.SpaceEvenly,
-                horizontalAlignment = Alignment.CenterHorizontally
+                modifier =
+                    Modifier
+                        .constrainAs(contentRef) {
+                            top.linkTo(toolbarRef.bottom, MaterialTheme.dimens.smallGap)
+                            start.linkTo(parent.start)
+                            end.linkTo(parent.end)
+                            bottom.linkTo(parent.bottom, MaterialTheme.dimens.smallGap)
+                            width = Dimension.fillToConstraints
+                            height = Dimension.fillToConstraints
+                        }.verticalScroll(rememberScrollState())
+                        .padding(
+                            horizontal = MaterialTheme.dimens.defaultGap,
+                            vertical = MaterialTheme.dimens.largePadding,
+                        ),
+                verticalArrangement = Arrangement.SpaceBetween,
+                horizontalAlignment = Alignment.CenterHorizontally,
             ) {
-
-                HorizontalUserAccounts(
-                    modifier = Modifier,
-                    accounts = state.userAccounts,
-                    onIbanIconClicked = {},
-                    onAccountNumberIconClicked = {},
-                    onBalanceIconClicked = {},
-                    onSettingIconClicked = {}
-                )
+                if (state.userAccounts.isEmpty()) {
+                    AppShimmer(
+                        modifier =
+                            Modifier
+                                .fillMaxSize()
+                                .height(180.dp),
+                    )
+                } else {
+                    HorizontalUserAccounts(
+                        modifier = Modifier,
+                        accounts = state.userAccounts,
+                        onIbanIconClicked = {},
+                        onAccountNumberIconClicked = {},
+                        onBalanceIconClicked = {},
+                        onSettingIconClicked = {},
+                    )
+                }
 
                 FrequentFeatures(
                     modifier = Modifier.fillMaxWidth(),
-                    features = state.frequentFeatures
+                    features = state.frequentFeatures,
                 )
 
                 BannerList(
                     modifier = Modifier.fillMaxWidth(),
-                    banners = state.banners
+                    banners = state.banners,
                 )
 
                 BankFeatures(
                     modifier = Modifier.fillMaxWidth(),
-                    features = state.bankFeatures
+                    features = state.bankFeatures,
                 )
 
                 CarFeatures(
                     modifier = Modifier.fillMaxWidth(),
-                    features = state.carFeatures
+                    features = state.carFeatures,
                 )
-
             }
-
         }
-
     }
 
     @Composable
-    private fun CarFeatures(modifier: Modifier, features: List<FeatureUiModel>) {
+    private fun CarFeatures(
+        modifier: Modifier,
+        features: List<FeatureUiModel>,
+    ) {
         HorizontalFeatureList(
             actionIcon = com.sweet.iva.core.designsystem.R.drawable.ic_edit,
             name = "خدمات خودرو",
             modifier = modifier,
             features = features,
-            onFeatureClicked = {})
+            onFeatureClicked = {},
+        )
     }
 
     @Composable
-    private fun BankFeatures(modifier: Modifier, features: List<FeatureUiModel>) {
+    private fun BankFeatures(
+        modifier: Modifier,
+        features: List<FeatureUiModel>,
+    ) {
         HorizontalFeatureList(
             containerColor = BlueRoyal100,
             actionIcon = com.sweet.iva.core.designsystem.R.drawable.ic_edit,
             name = "خدمات ویژه بانکی",
             modifier = modifier,
             features = features,
-            onFeatureClicked = {})
-    }
-
-    @Composable
-    private fun BannerList(modifier: Modifier, banners: List<BannerUiModel>) {
-        HorizontalBannerList(
-            modifier = modifier.fillMaxWidth(),
-            banners = banners,
-            onBannerClicked = {}
+            onFeatureClicked = {},
         )
     }
 
     @Composable
-    private fun FrequentFeatures(modifier: Modifier, features: List<FeatureUiModel>) {
+    private fun BannerList(
+        modifier: Modifier,
+        banners: List<BannerUiModel>,
+    ) {
+        HorizontalBannerList(
+            modifier = modifier.fillMaxWidth(),
+            banners = banners,
+            onBannerClicked = {},
+        )
+    }
+
+    @Composable
+    private fun FrequentFeatures(
+        modifier: Modifier,
+        features: List<FeatureUiModel>,
+    ) {
         HorizontalFeatureList(
             actionIcon = com.sweet.iva.core.designsystem.R.drawable.ic_edit,
             name = "خدمات پر کاربرد",
             modifier = modifier,
             features = features,
-            onFeatureClicked = {})
+            onFeatureClicked = {},
+        )
     }
 
     @Composable
@@ -174,32 +194,25 @@ class DashboardScreen : BaseScreen<DashboardUiModel, DashboardAction, DashboardE
         onLeftIconClicked: () -> Unit,
         onRightIconClicked: () -> Unit,
     ) {
-
         AppToolbar(
             modifier = modifier,
             toolbarTitle = "ایوا",
             onLeftIconClicked = onLeftIconClicked,
             leftIcon = R.drawable.ic_person,
             rightIcon = R.drawable.ic_more,
-            onRightIconClicked = onRightIconClicked
+            onRightIconClicked = onRightIconClicked,
         )
-
     }
 
     @ThemePreviews
     @Composable
     fun DashboardContent() {
-
         AppTheme {
-
             AppBackground(modifier = Modifier) {
-
             }
             Content(
-                state = DashboardUiModel()
+                state = DashboardUiModel(),
             )
-
         }
-
     }
 }
--- feature/home/src/main/java/com/sweet/iva/feature/home/dashboard/viewmodel/DashboardViewModel.kt
+++ feature/home/src/main/java/com/sweet/iva/feature/home/dashboard/viewmodel/DashboardViewModel.kt
@@ -1,103 +1,106 @@
 package com.sweet.iva.feature.home.dashboard.viewmodel
 
-import androidx.lifecycle.viewModelScope
-import com.sweet.arch.core.domain.infra.cache.ReactiveCache
-import com.sweet.arch.core.domain.model.user.User
 import com.sweet.iva.core.designsystem.component.model.UserCardUiModel
 import com.sweet.iva.core.ui.viewmodel.BaseViewModel
 import com.sweet.iva.feature.home.dashboard.model.DashboardAction
 import com.sweet.iva.feature.home.dashboard.model.DashboardEvent
 import com.sweet.iva.feature.home.dashboard.model.DashboardUiModel
+import com.sweet.iva.feature.home.dashboard.model.mockAccounts
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class DashboardViewModel @Inject constructor(
-    private val userCache: ReactiveCache<User>
-) :
+class DashboardViewModel
+    @Inject
+    constructor() :
     BaseViewModel<DashboardUiModel, DashboardAction, DashboardEvent>(
-        initialState = DashboardUiModel()
-    ) {
-
-    override fun handleAction(action: DashboardAction) {
-        when (action) {
-            is DashboardAction.PanChanged -> changeCardPan(action.cardIndex, action.pan)
-            is DashboardAction.NameChanged -> changeCardName(action.cardIndex, action.name)
-            is DashboardAction.MonthChanged -> changeCardMonth(action.cardIndex, action.month)
-            is DashboardAction.YearChanged -> changeCardYear(action.cardIndex, action.year)
-        }
-    }
-
-    private fun changeCardYear(cardIndex: Int, year: String) {
-        updateState {
-            it.copy(
-                userCards = updateUserCardAtIndex(it.userCards, cardIndex) { oldCard ->
-                    oldCard.copy(year = year)
-                }
-            )
-        }
-    }
-
-    private fun changeCardMonth(cardIndex: Int, month: String) {
-        updateState {
-            it.copy(
-                userCards = updateUserCardAtIndex(it.userCards, cardIndex) { oldCard ->
-                    oldCard.copy(
-                        month = month
-                    )
-                }
-            )
-        }
-    }
-
-    private fun changeCardName(index: Int, name: String) {
-        updateState {
-            it.copy(
-                userCards = updateUserCardAtIndex(it.userCards, index) { oldCard ->
-                    oldCard.copy(name = name)
-                }
-            )
-        }
-    }
-
-    private fun updateUserCardAtIndex(
-        source: List<UserCardUiModel>,
-        index: Int,
-        update: (card: UserCardUiModel) -> UserCardUiModel
-    ): List<UserCardUiModel> {
-
-        return mutableListOf<UserCardUiModel>().apply {
-
-            addAll(source)
-            removeAt(index)
-            add(index, update.invoke(source[index]))
-
+            initialState = DashboardUiModel(),
+        ) {
+        override fun handleAction(action: DashboardAction) {
+            when (action) {
+                is DashboardAction.PanChanged -> changeCardPan(action.cardIndex, action.pan)
+                is DashboardAction.NameChanged -> changeCardName(action.cardIndex, action.name)
+                is DashboardAction.MonthChanged -> changeCardMonth(action.cardIndex, action.month)
+                is DashboardAction.YearChanged -> changeCardYear(action.cardIndex, action.year)
+            }
         }
 
-    }
-
-    private fun isPanValid(pan: String): Boolean {
-        return pan.length <= 16
-    }
-
-    private fun changeCardPan(index: Int, pan: String) {
-
-        updateState {
-            it.copy(
-                userCards = updateUserCardAtIndex(it.userCards, index) { oldCard ->
-                    oldCard.copy(pan = pan)
-                }
-            )
+        private fun changeCardYear(
+            cardIndex: Int,
+            year: String,
+        ) {
+            updateState {
+                it.copy(
+                    userCards =
+                        updateUserCardAtIndex(it.userCards, cardIndex) { oldCard ->
+                            oldCard.copy(year = year)
+                        },
+                )
+            }
         }
 
-    }
-
-    fun test() {
-
-        viewModelScope.launch {
-            userCache.clear()
+        private fun changeCardMonth(
+            cardIndex: Int,
+            month: String,
+        ) {
+            updateState {
+                it.copy(
+                    userCards =
+                        updateUserCardAtIndex(it.userCards, cardIndex) { oldCard ->
+                            oldCard.copy(
+                                month = month,
+                            )
+                        },
+                )
+            }
         }
 
+        private fun changeCardName(
+            index: Int,
+            name: String,
+        ) {
+            updateState {
+                it.copy(
+                    userCards =
+                        updateUserCardAtIndex(it.userCards, index) { oldCard ->
+                            oldCard.copy(name = name)
+                        },
+                )
+            }
+        }
+
+        private fun updateUserCardAtIndex(
+            source: List<UserCardUiModel>,
+            index: Int,
+            update: (card: UserCardUiModel) -> UserCardUiModel,
+        ): List<UserCardUiModel> =
+            mutableListOf<UserCardUiModel>().apply {
+                addAll(source)
+                removeAt(index)
+                add(index, update.invoke(source[index]))
+            }
+
+        private fun isPanValid(pan: String): Boolean = pan.length <= 16
+
+        private fun changeCardPan(
+            index: Int,
+            pan: String,
+        ) {
+            updateState {
+                it.copy(
+                    userCards =
+                        updateUserCardAtIndex(it.userCards, index) { oldCard ->
+                            oldCard.copy(pan = pan)
+                        },
+                )
+            }
+        }
+
+        fun test() {
+            updateState {
+                it.copy(
+                    userAccounts = mockAccounts,
+                )
+            }
+        }
     }
-}
