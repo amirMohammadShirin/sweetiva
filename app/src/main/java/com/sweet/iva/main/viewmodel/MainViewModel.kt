86484dc97753af09582a3a1c943efebce499968a
amirmohammadshirin <Amirmohamad.Shirin@snapp.cab>
2024-08-12 16:14:48 +0330

    change start destination logic

--- app/src/main/java/com/sweet/iva/main/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/sweet/iva/main/viewmodel/MainViewModel.kt
@@ -2,30 +2,23 @@ package com.sweet.iva.main.viewmodel
 
 import androidx.lifecycle.viewModelScope
 import com.sweet.arch.core.domain.model.user.User
-import com.sweet.arch.core.domain.usecase.user.GetCurrentUserUseCase
 import com.sweet.arch.core.domain.usecase.user.ListenToCurrentUserUseCase
-import com.sweet.iva.core.common.dispatcher.DispatcherProvider
 import com.sweet.iva.core.ui.navigation.ApplicationRoutes
 import com.sweet.iva.core.ui.viewmodel.BaseViewModel
 import com.sweet.iva.main.model.MainEvent
 import com.sweet.iva.main.model.MainViewState
+import kotlinx.coroutines.flow.buffer
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-internal class MainViewModel
-@Inject
-constructor(
-    private val dispatcherProvider: DispatcherProvider,
+internal class MainViewModel @Inject constructor(
     private val listenToCurrentUserUseCase: ListenToCurrentUserUseCase,
-    private val getCurrentUserUseCase: GetCurrentUserUseCase,
 ) : BaseViewModel<MainViewState, MainEvent>(initialState = MainViewState()) {
 
     fun start() {
         viewModelScope.launch {
-            val currentUser = getCurrentUserUseCase.execute(null)
             collectCurrentUser()
-            if (currentUser == null) navigate(null)
         }
     }
 
@@ -33,6 +26,7 @@ constructor(
         viewModelScope.launch {
             listenToCurrentUserUseCase
                 .start(null)
+                .buffer()
                 .collectLatest { user ->
                     navigate(user)
                 }
@@ -46,7 +40,15 @@ constructor(
         updateState {
             it.copy(
                 startDestination = destination,
-                user = currentUser,
+                user = currentUser
+            )
+        }
+        hideSplash()
+    }
+
+    private fun hideSplash() {
+        updateState {
+            it.copy(
                 loading = false
             )
         }
